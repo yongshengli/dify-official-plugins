44d5f8aa9585c109f8385dfb709c32d08cfc4180
Yeuoly <admin@srmxy.cn>
2025-02-26 17:47:43 +0800

    plugin: feishu

--- migrations/tools/feishu/provider/feishu.py
+++
@@ -1,7 +0,0 @@
-from tools.feishu_group_bot import FeishuGroupBotTool
-from dify_plugin import ToolProvider
-
-
-class FeishuProvider(ToolProvider):
-    def _validate_credentials(self, credentials: dict) -> None:
-        FeishuGroupBotTool()
